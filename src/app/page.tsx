de579d7abc2b228b1ea1eb8e5a2566fa20379f24
arhappy777 <arhappy777@gmail.com>
2026-03-30 18:52:50 +0300

    feat: add bg image back

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,14 @@ import { Quiz } from "@/components/Quiz";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-center p-4">
-      <Quiz />
+    <main className="relative min-h-screen flex items-center justify-center p-4 overflow-hidden">
+      <div className="fixed inset-0 z-0">
+        <div className="absolute inset-0 bg-[url('/bg/bg1.jpg')] bg-cover bg-center" />
+        <div className="absolute inset-0 bg-black/60" />
+      </div>
+      <div className="relative z-10">
+        <Quiz />
+      </div>
     </main>
   );
 }
